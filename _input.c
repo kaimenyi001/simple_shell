76ace1933ec9068b6312a3533c18018ade2c0859
kaimenyi001 <deniskaimenyi1@gmail.com>
2023-09-20 01:23:30 -0700

    checker

--- _input.c
+++ _input.c
@@ -9,7 +9,7 @@
 char *_input(size_t *buff_size)
 {
 	ssize_t ger;
-	char *buff = NULL;
+	char *buff = NULL, **env;
 
 	ger = getline(&buff, buff_size, stdin);
 	if (ger == -1)
@@ -18,16 +18,22 @@ char *_input(size_t *buff_size)
 		free(buff);
 		exit(EXIT_FAILURE);
 	}
-
 	while (*buff == ' ' || *buff == '\t' || *buff == '\n')
 		buff++;
-
 	if (_strcmp(buff, "exit\n") == 0)
 	{
 		free(buff);
 		exit(EXIT_SUCCESS);
 	}
-
-
+	else if (_strcmp(buff, "env\n") == 0)
+	{
+		env = environ;
+		while (*env != NULL)
+		{
+			write(STDOUT_FILENO, *env, _strlen(*env));
+			write(STDOUT_FILENO, "\n", 1);
+			env++;
+		}
+	}
 	return (buff);
 }
